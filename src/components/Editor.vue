9bcf181a51e4c59a8d84c2338688eb913b3f4520
Dominik Schmidt <dev@dominik-schmidt.de>
2023-09-21 18:20:07 +0200

    Reproduce issue

---
+++ src/components/Editor.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router';
+
+defineProps<{ msg: string }>()
+
+const $route = useRoute()
+</script>
+
+<template>
+  <h1>EDITOR</h1>
+  <h2>{{ $route.params.path }}</h2>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Editor from './components/Editor.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const router = createRouter({
+    history: createWebHistory('/'),
+    routes: [
+        {
+            path: '/open/:path(.*)?',
+            component: Editor,
+            children: []
+        }
+    ]
+})
+app.use(router)
+
+app.mount('#app')
