ec3e78f84f8ba76ded983f1128d5d6d7cafe7c61
Dominik Schmidt <dev@dominik-schmidt.de>
2023-09-21 18:34:51 +0200

    Add workaround

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+// workaround:
+import history from 'connect-history-api-fallback'
+import { existsSync } from 'fs'
+import { join } from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    // ***********************************
+    // COMMENT IN TO ENABLE WORKAROUND
+    // ***********************************
+    // {
+    //   name: 'fallback-to-index-html',
+    //   configureServer(server) {
+    //     return () => {
+    //       const handler = history({
+    //         disableDotRule: true,
+    //         rewrites: [
+    //           {
+    //             from: /$/,
+    //             to({ parsedUrl, request }) {
+    //               const root = "."
+    //               const decodedPath = decodeURIComponent(parsedUrl.pathname)
+    //               const rewritten = decodedPath + 'index.html'
+    //               console.log('decodedPath', decodedPath, 'rewritten', rewritten)
+    //               if (decodedPath !== '/' && existsSync(join(root, decodedPath))) {
+    //                 return decodedPath
+    //               }
+    //               if (existsSync(join(root, rewritten))) {
+    //                 return rewritten
+    //               }
+    //               return `/index.html`
+    //             }
+    //           }
+    //         ]
+    //       })
+
+    //       server.middlewares.use(handler)
+    //     }
+    //   }
+    // }
+  ],
 })
